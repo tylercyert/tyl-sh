e2f2943f396b5c2525c33fe5274e273613bf4418
Tyler <35160321+tylercyert@users.noreply.github.com>
2026-04-07 16:55:41 -0500

    Change greeting from 'Hello!' to 'hello world!'

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <section>
-        <h1>Hello!</h1>
+        <h1>hello world!</h1>
         <p>My name is Tyler Cyert. By day, I am a Systems Engineer at SoundPro.com. By night, I am a developer, writer, and musician.</p>
       </section>
 
